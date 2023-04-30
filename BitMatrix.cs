d37ccf351e91bde0d07fe99e1b5272557241fd88
rufus_alpha <jkb.millan98@gmail.com>
2023-04-30 14:09:08 +0200

    added additional Constructors"

--- BitMatrix.cs
+++ BitMatrix.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 // prostokątna macierz bitów o wymiarach m x n
 
@@ -12,6 +13,12 @@ namespace BitMatrixSpace
         public int NumberOfColumns { get; }
         public bool IsReadOnly => false;
 
+        public bool this[int i, int j]
+        {
+            get => data[i * NumberOfColumns + j];
+            set => data[i * NumberOfColumns + j] = value;
+        }
+
         // tworzy prostokątną macierz bitową wypełnioną `defaultValue`
         public BitMatrix(int numberOfRows, int numberOfColumns, int defaultValue = 0)
         {
@@ -20,6 +27,61 @@ namespace BitMatrixSpace
             data = new BitArray(numberOfRows * numberOfColumns, BitToBool(defaultValue));
             NumberOfRows = numberOfRows;
             NumberOfColumns = numberOfColumns;
+        }
+
+        public BitMatrix(int numberOfRows, int numberOfColumns, params int[] bits)
+        {
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+            data = new BitArray(numberOfRows * numberOfColumns, false);
+
+           if( bits is not null && bits.Length > 1)
+            {
+                for (int i = 0; i < numberOfRows * numberOfColumns; i++)
+                {
+                    data[i] = i < bits.Length ? BitToBool(bits[i]) : false;
+                }
+            }
+        }
+
+        public BitMatrix(int[,] bits)
+        {
+            if (bits is null)
+                throw new NullReferenceException();
+            if (bits.Length <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            NumberOfRows = bits.GetLength(0);
+            NumberOfColumns = bits.GetLength(1);
+
+            data = new BitArray(NumberOfRows * NumberOfColumns, false);
+
+            int index = 0;
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfColumns; j++)
+                    data[index++] = BitToBool(bits[i, j]);
+            }
+        }
+
+        public BitMatrix(bool[,] bits)
+        {
+            if (bits is null)
+                throw new NullReferenceException();
+            if (bits.Length <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            NumberOfRows = bits.GetLength(0);
+            NumberOfColumns = bits.GetLength(1);
+
+            data = new BitArray(NumberOfRows * NumberOfColumns, false);
+
+            int index = 0;
+            for (int i = 0; i < NumberOfRows; i++)
+            {
+                for (int j = 0; j < NumberOfColumns; j++)
+                    data[index++] = bits[i, j];
+            }
         }
 
         public static int BoolToBit(bool boolValue) => boolValue ? 1 : 0;
--- Program.cs
+++ Program.cs
@@ -9,13 +9,29 @@ namespace Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
-
+            // konstruktor BitMatrix(int, int, params int[])
+            // macierz 2x2, komplet danych w tablicy
             var m = new BitMatrix(2, 2, new int[] { 1, 0, 0, 1 });
             Console.WriteLine(m);
 
             m = new BitMatrix(2, 2, 1, 0, 0, 0);
             Console.WriteLine(m);
+
+            // konstruktor BitMatrix(int, int, params int[])
+            // macierz 2x2, za dużo danych w tablicy
+            m = new BitMatrix(2, 2, 1, 0, 0, 1, 1, 1, 0);
+            Console.WriteLine(m);
+
+            // macierz 3x2, za mało danych w tablicy
+            m = new BitMatrix(3, 2, 1, 0, 0, 1, 1);
+            Console.WriteLine(m);
+
+            // konstruktor BitMatrix(int[,])
+            //int[,] arr = new int[,] { { 1, 0, 1 }, { 0, 1, 1 } };
+            //m = new BitMatrix(arr);
+            //Console.WriteLine(arr.GetLength(0) == m.NumberOfRows);
+            //Console.WriteLine(arr.GetLength(1) == m.NumberOfColumns);
+            //Console.Write(m.ToString());
         }
        
     }
